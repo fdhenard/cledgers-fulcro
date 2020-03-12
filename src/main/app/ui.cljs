d9a9aa164f8e2fbf135ce46de8b239352869c43f
Frank Henard <fdhenard@gmail.com>
2020-03-12 11:40:17 -0500

    separate TransactionList into separate component

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -62,37 +62,34 @@
 
 (def ui-transaction (comp/factory Transaction {:keyfn :transaction/id}))
 
-(defsc Root #_[this {:keys [friends enemies]}]
-  [this {:root/keys [transactions]}]
-  ;; {:query [{:friends (comp/get-query PersonList)}
-  ;;          {:enemies (comp/get-query PersonList)}]
-  ;;  :initial-state (fn [params]
-  ;;                   {:friends (comp/get-initial-state PersonList {:id :friends
-  ;;                                                                 :label "Friends"})
-  ;;                    :enemies (comp/get-initial-state PersonList {:id :enemies
-  ;;                                                                 :label "Enemies"})})}
-  {:query [{:root/transactions (comp/get-query Transaction)}]
-   :initial-state
-   (fn [params]
-     {:root/transactions
-      [(comp/get-initial-state Transaction {:id 1
-                                            :date "2020"
-                                            :payee "test payee"
-                                            :ledger "test ledger"
-                                            :description "soem descrip"
-                                            :amount "1111.11"})]})}
+(defsc TransactionList [this {:transaction-list/keys [transactions] :as props}]
+  {:query [{:transaction-list/transactions (comp/get-query Transaction)}]
+   :ident (fn [this props] [:component/id ::transaction-list])
+   :initial-state (fn [this props]
+                    {:transaction-list/transactions [(comp/get-initial-state Transaction {:id 1
+                                                                                          :date "2020"
+                                                                                          :payee "test payee"
+                                                                                          :ledger "test ledger"
+                                                                                          :description "soem descrip"
+                                                                                          :amount "1111.11"})]})}
   (dom/div
    (dom/table :.table
-   (dom/thead
-    (dom/tr
-     (dom/th "date")
-     (dom/th "payee")
-     (dom/th "ledger")
-     (dom/th "desc")
-     (dom/th "amount")
-     (dom/th "controls")))
-   (dom/tbody
-    (map ui-transaction transactions)))
-   #_(ui-transaction-list transactions)
-   #_(ui-person-list friends)
-   #_(ui-person-list enemies)))
+              (dom/thead
+               (dom/tr
+                (dom/th "date")
+                (dom/th "payee")
+                (dom/th "ledger")
+                (dom/th "desc")
+                (dom/th "amount")
+                (dom/th "controls")))
+              (dom/tbody
+               (map ui-transaction transactions)))))
+
+(def ui-transaction-list (comp/factory TransactionList))
+
+(defsc Root
+  [this {:root/keys [transaction-list]}]
+  {:query [{:root/transaction-list (comp/get-query TransactionList)}]
+   :initial-state (fn [this] {:root/transaction-list (comp/get-initial-state TransactionList)})}
+  (dom/div
+   (ui-transaction-list transaction-list)))
