457b9ea5ec4b307ba3402b91b28ffc77c296c037
Frank Henard <fdhenard@gmail.com>
2020-10-09 15:36:01 -0500

    be more functional with mutation

--- src/main/cledgers_fulcro/mutations_client.cljs
+++ src/main/cledgers_fulcro/mutations_client.cljs
@@ -4,34 +4,29 @@
             [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
             [cledgers-fulcro.utils.utils :as utils]))
 
+(defn add-xaction-func [state-map new-xaction]
+  (let [new-xaction-key
+        [:cledgers-fulcro.models.transaction/id (:id new-xaction)]
+        date-previous (:cledgers-fulcro.models.transaction/date new-xaction)
+        new-new-xaction (utils/new-xaction date-previous)
+        new-new-xaction-id (:new-transaction/id new-new-xaction)]
+    (-> state-map
+        (assoc-in new-xaction-key new-xaction)
+        (update-in [:component/id
+                    :cledgers-fulcro.ui.core/transaction-list
+                    :transaction-list/transactions]
+                   #(conj % new-xaction-key))
+        (assoc-in [:new-transaction/id new-new-xaction-id] new-new-xaction)
+        (assoc-in [:component/id
+                   :cledgers-fulcro.ui.core/transaction-list
+                   :transaction-list/new-transaction]
+                  [:new-transaction/id new-new-xaction-id]))))
+
 (defmutation add-transaction
     [{:keys [id] :as mut-in}]
   (action [{:keys [state] :as action-in}]
     (let [#_ (pp/pprint {:muts-client--add-xaction
                          { ;; :state @state
                           :xaction-in mut-in}})
-          new-xaction-key [:cledgers-fulcro.models.transaction/id id]
-          _ (swap! state assoc-in new-xaction-key mut-in)
-          _ (swap!
-             state
-             update-in
-             [:component/id
-              :cledgers-fulcro.ui.core/transaction-list
-              :transaction-list/transactions]
-             #(conj % new-xaction-key))
-          date-previous (:cledgers-fulcro.models.transaction/date mut-in)
-          new-new-xaction (utils/new-xaction date-previous)
-          new-new-xaction-id (:new-transaction/id new-new-xaction)
-          _ (swap!
-             state
-             assoc-in
-             [:new-transaction/id new-new-xaction-id]
-             new-new-xaction)
-          _ (swap!
-             state
-             assoc-in
-             [:component/id
-              :cledgers-fulcro.ui.core/transaction-list
-              :transaction-list/new-transaction]
-             [:new-transaction/id new-new-xaction-id])]))
+          _ (swap! state add-xaction-func mut-in)]))
   (remote [env] true))
